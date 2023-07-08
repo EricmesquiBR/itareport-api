0752ad7cd00745f6dd8842ab9add3d0b3805cb79
Theus1990 <matheusrabelo9@hotmail.com>
2023-07-08 16:38:47 -0300

    Permitir criação da categoria

--- src/api/categoryRoute.js
+++ src/api/categoryRoute.js
@@ -3,6 +3,7 @@ const categoryController = require("../controllers/categoryController");
 const express = require("express");
 const router = express.Router();
 
+router.post("/category/:id", categoryController.createCategorys)
 router.get("/category", categoryController.findAllCategory);
 router.get("/category/:id", categoryController.findReportByCategoryId);
 
